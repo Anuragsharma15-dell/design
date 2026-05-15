a3068360e4674b6340d12092eb0fe2b587489ffb
anurag <yourboyanurag777@gmail.com>
2026-05-15 15:25:37 +0530

    Added signin

--- client/client/src/pages/Signin.jsx
+++ client/client/src/pages/Signin.jsx
@@ -1,9 +1,27 @@
-
+import axios from "axios"
 
 
 
 export const Signin = () => {
 
+
+const Backendurl ="https://localhost:3000/google/auth";
+
+const result  = axios.post(Backendurl, {withCredentials: true});
+const response  = result.json();
+if(!response){
+    return 
+}
+
+
+
+
+    return (
+        <div>login with google</div>
+    )
+
+
+}
 
 
 
